855b632cd12e50ebfe4dc80563fe3e655bbb0dd4
natic <natic@163.com>
2017-10-22 15:56:06 +0800

    remove useless bower files form folder

--- angular1/gulpfile.js
+++ angular1/gulpfile.js
@@ -4,6 +4,7 @@ var gulp = require('gulp');
 // plugins
 var connect = require('gulp-connect');
 var jshint = require('gulp-jshint');
+var bower = require('gulp-bower');
 var uglify = require('gulp-uglify');
 var minifyCSS = require('gulp-minify-css');
 var clean = require('gulp-clean');
@@ -22,24 +23,20 @@ gulp.task('clean', function() {
 });
 gulp.task('minify-css', function() {
     var opts = {comments:true,spare:true};
-    gulp.src(['./app/**/*.css', '!./app/bower_components/**'])
+    gulp.src(['./app/**/*.css'])
         //.pipe(minifyCSS(opts))
         .pipe(gulp.dest('./dist/'))
 });
-gulp.task('concat-js', function () {
-    gulp.src(['./app/bower_components']);
-});
 gulp.task('minify-js', function() {
-    gulp.src(['./app/**/*.js', '!./app/bower_components/**'])
+    gulp.src(['./app/**/*.js'])
         /*.pipe(uglify({
             // inSourceMap:
             // outSourceMap: "app.js.map"
         }))*/
         .pipe(gulp.dest('./dist/'))
 });
-gulp.task('copy-bower-components', function () {
-    gulp.src('./app/bower_components/**')
-        .pipe(gulp.dest('dist/bower_components'));
+gulp.task('bower', function () {
+    return bower();
 });
 gulp.task('copy-html-files', function () {
     gulp.src('./app/**/*.html')
@@ -71,7 +68,7 @@ gulp.task('build', function(cb) {
             'minify-css',
             'minify-js',
             'copy-html-files',
-            'copy-bower-components',
+            'bower',
             'connectDist'
         ],
         cb
