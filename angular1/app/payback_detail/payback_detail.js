79a43423323b239cdef342b580ad3557ca3620a5
Xshaoyi <625799527@qq.com>
2017-11-06 09:29:39 +0800

    payback_detail

---
+++ angular1/app/payback_detail/payback_detail.js
@@ -0,0 +1,12 @@
+angular.module('p2p.payback_detail',['ui.router'])
+    .config(['$stateProvider', function ($stateProvider) {
+        var p2pState = {name:'p2p.payback_detail',url:'/payback_detail',
+            templateUrl: 'payback_detail/payback_detail.html',
+            controller: 'payback_detail'};
+        $stateProvider.state(p2pState);
+    }])
+    .controller('payback_detail', payback_detail);
+p2pCtrl.$inject = ['$scope', '$location', 'RestService'];
+function payback_detail($scope, $location, RestService) {
+
+}
--- angular1/app/payback_trend/payback_trend.js
+++ angular1/app/payback_trend/payback_trend.js
@@ -5,8 +5,8 @@ angular.module('p2p.payback_trend',['ui.router'])
             controller: 'payback_trendCtrl'};
         $stateProvider.state(p2pState);
     }])
-    .controller('payback_trend', payback_trend);
+    .controller('payback_trendCtrl', payback_trendCtrl);
 p2pCtrl.$inject = ['$scope', '$location', 'RestService'];
-function payback_trend($scope, $location, RestService) {
+function payback_trendCtrl($scope, $location, RestService) {
 
 }
